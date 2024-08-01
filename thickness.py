fd5474330f48ade0a55032a7950ed13b49fed5bc
Mia <169999458+mia-dev-aus@users.noreply.github.com>
2024-08-01 13:30:53 +1000

    Update thickness.py

--- thickness.py
+++ thickness.py
@@ -1,9 +1,8 @@
-import math
 OPTICAL_PATH = 1.4276
-n = 1.5
-
+N = 1.5
+# calculate thickness of the glass block required to introduce a phase difference
 def calculate_t (n, path_diff):
-    t = path_diff / (n - 1);
+    t = path_diff / (n - 1)
     return t
 
-print("The thickness required by n =",n," is t =", calculate_t(n, OPTICAL_PATH))
+print("The thickness required by n =",N," is t =", calculate_t(N, OPTICAL_PATH))
